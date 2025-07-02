6fe65f5114729fbe4b932269494aa176b19a3746
gurrman22 <79734934+gurrman22@users.noreply.github.com>
2025-07-03 10:59:33 +1200

    Update BackgroudDownloadscript.ps1

--- BackgroudDownloadscript.ps1
+++ BackgroudDownloadscript.ps1
@@ -1,7 +1,7 @@
-﻿$url1 = "https://raw.githubusercontent.com/gurrman22/VR/refs/heads/main/Background.jpg"
-$DesktopImageValue = "C:\MDM\Desktop.jpg"
-$url2 = "https://raw.githubusercontent.com/gurrman22/VR/refs/heads/main/Background.jpg"
-$LockscreenImageValue = "C:\MDM\Lockscreen.jpg"
+﻿$url1 = "https://raw.githubusercontent.com/gurrman22/VR/refs/heads/main/Background1.jpg"
+$DesktopImageValue = "C:\MDM\Desktop1.jpg"
+$url2 = "https://raw.githubusercontent.com/gurrman22/VR/refs/heads/main/Lockscreen1.jpg"
+$LockscreenImageValue = "C:\MDM\Lockscreen1.jpg"
 $directory = "C:\MDM\"
 
 
@@ -13,7 +13,3 @@ If ((Test-Path -Path $directory) -eq $false)
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url1, $DesktopImageValue)
 $wc.DownloadFile($url2, $LockscreenImageValue)
-
-# local path of images
-$WallpaperLocalIMG = "C:\MDM\Desktop.png"
-$LockscreenLocalIMG = "C:\MDM\Lockscreen.png"
